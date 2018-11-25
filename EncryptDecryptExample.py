1e663abe6ac88aec7675b68730ebeb16175deae6
Ducc <43486978+BlackmanWhite@users.noreply.github.com>
2018-11-25 15:08:33 -0200

    Fixed naming error

--- EncryptDecryptExample.py
+++ EncryptDecryptExample.py
@@ -5,11 +5,11 @@ from pysCrypt import pysCrypt
 
 msg = 'Hello world!'
 
-cyph = sCrypt.encrypt(msg, 256)
-s_cyph = sCrypt.simple_encrypt(msg, 256)
-se_cyph = sCrypt.secure_encrypt(msg, 256)
+cyph = pysCrypt.encrypt(msg, 256)
+s_cyph = pysCrypt.simple_encrypt(msg, 256)
+se_cyph = pysCrypt.secure_encrypt(msg, 256)
 print(cyph + '\n' + s_cyph + '\n' + se_cyph)
 
-print('\nDecrypt:', sCrypt.decrypt(cyph, 256))
-print('Simple decrypt:', sCrypt.simple_decrypt(s_cyph, 256))
-print('Secure decrypt:' , sCrypt.secure_decrypt(se_cyph, 256))
+print('\nDecrypt:', pysCrypt.decrypt(cyph, 256))
+print('Simple decrypt:', pysCrypt.simple_decrypt(s_cyph, 256))
+print('Secure decrypt:' , pysCrypt.secure_decrypt(se_cyph, 256))
