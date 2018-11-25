d448fc53075cf4a76011062e0078ec5f7f4b698a
Ducc <43486978+BlackmanWhite@users.noreply.github.com>
2018-11-25 15:59:13 -0200

    Example naming fix

--- EncryptDecryptExample.py
+++ EncryptDecryptExample.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-from pysCrypt import pysCrypt
+from pysCrypto import pysCrypto
 
 msg = 'Hello world!'
 
-cyph = pysCrypt.encrypt(msg, 256)
-s_cyph = pysCrypt.simple_encrypt(msg, 256)
-se_cyph = pysCrypt.secure_encrypt(msg, 256)
+cyph = pysCrypto.encrypt(msg, 256)
+s_cyph = pysCrypto.simple_encrypt(msg, 256)
+se_cyph = pysCrypto.secure_encrypt(msg, 256)
 print(cyph + '\n' + s_cyph + '\n' + se_cyph)
 
-print('\nDecrypt:', pysCrypt.decrypt(cyph, 256))
-print('Simple decrypt:', pysCrypt.simple_decrypt(s_cyph, 256))
-print('Secure decrypt:' , pysCrypt.secure_decrypt(se_cyph, 256))
+print('\nDecrypt:', pysCrypto.decrypt(cyph, 256))
+print('Simple decrypt:', pysCrypto.simple_decrypt(s_cyph, 256))
+print('Secure decrypt:' , pysCrypto.secure_decrypt(se_cyph, 256))
